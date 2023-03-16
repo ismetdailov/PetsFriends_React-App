3c6b07daaa9110c28319904326f30c6411b75d0f
Ismet Dailov <60391985+ismetdailov@users.noreply.github.com>
2023-03-17 00:55:51 +0200

    initial and create
    add some folders to structured app

---
+++ src/components/Home/GuestHome/Guest.js
@@ -0,0 +1,14 @@
+import styles from "../GuestHome/GuestStyle.module.css"
+function Guest() {
+    return (
+        <>
+            <div className="guest">
+               <h1>Hello</h1>
+            </div>
+            <footer>
+                <h5>2023</h5>
+            </footer>
+        </>
+    );
+}
+export default Guest
